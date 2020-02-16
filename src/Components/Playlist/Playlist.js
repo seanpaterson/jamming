23da86fd6a0ec64dab23dcab9806063d4d5af694
Sean <seanpaterson117@gmail.com>
2020-02-15 16:35:16 -0800

    Initial Components

---
+++ src/Components/Playlist/Playlist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Playlist.css';
+
+function Playlist() {
+    return (
+        <div className="Playlist">
+        <input value="New Playlist"/>
+        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        </div>
+    );
+}
+
+export default Playlist;
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Track.css';
+
+function Track () {
+        return (
+            <div className="SearchResults">
+            <h2>Results</h2>
+            </div>
+        );
+}
+
+export default Track;
